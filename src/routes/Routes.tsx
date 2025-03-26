9cedecee1a3fd2703475a719bd334ebd8a52da09
Eunjin Oh <ibsuny3214@naver.com>
2025-03-26 14:39:30 +0900

    Feat: 라우터 수정 및 페이지 생성 (#2)

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,18 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ProjectPage from '@/views/main/page/ProjectPage';
+import ProjectPage from "@/views/main/page/ProjectPage";
+import SplashPage from "@/views/splash/page/SplashPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         {/* <Route path="/" element={</>}/> */}
-        <Route path="/" element={<ProjectPage />}>
-          {/* <Route path="project" element={</>}>
-            <Route path=":id" element={</>}/>
+        <Route path="/" element={<SplashPage />} />
+        <Route path="project" element={<ProjectPage />} />
+        {/*<Route path=":id" element={</>}/>
           </Route>
           <Route path="record" element={</>}/>
           <Route path="summary" element={</>}/> */}
-        </Route>
+
         {/* <Route path="/meeting" element={</>}/> */}
       </Routes>
     </BrowserRouter>
