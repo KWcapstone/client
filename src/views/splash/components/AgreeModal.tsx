7cad91d22078f3d295176b6d501dffe686e6bb16
Eunjin Oh <ibsuny3214@naver.com>
2025-03-30 16:54:57 +0900

    Feat: 동의 모달 로직 완성 (#2)

--- src/views/splash/components/AgreeModal.tsx
+++ src/views/splash/components/AgreeModal.tsx
@@ -12,6 +12,45 @@ interface AgreeModalProps {
 }
 
 const AgreeModal = ({ onCloseModal, onOpenSignup }: AgreeModalProps) => {
+  const [active, setActive] = useState(false);
+  const [allAgree, setAllAgree] = useState(false);
+  const [agree1, setAgree1] = useState(false);
+  const [agree2, setAgree2] = useState(false);
+  const [agree3, setAgree3] = useState(false);
+
+  const [agreeError, setAgreeError] = useState("");
+
+  const [agreeBtnShake, setAgreeBtnShake] = useState(false);
+
+  useEffect(() => {
+    setActive(agree1 && agree2 && agree3);
+  }, [agree1, agree2, agree3]);
+
+  useEffect(() => {
+    setAgreeError("");
+  }, [allAgree, agree1, agree2, agree3]);
+
+  // 가입하기 버튼 애니메이션
+  const triggerShake = () => {
+    setAgreeBtnShake(true);
+    setTimeout(() => setAgreeBtnShake(false), 500);
+  };
+
+  const handleAgreeClick = () => {
+    let hasError = false;
+    if (!agree1 || !agree2 || !agree3) {
+      setAgreeError("모두 동의해주세요.");
+      hasError = true;
+    }
+    if (hasError) {
+      triggerShake();
+      return;
+    }
+    // 가입하기 버튼 클릭 시 동작
+    console.log("가입하기 버튼 클릭");
+    // 여기서 가입하기 로직을 추가하세요
+  };
+
   return (
     <Modal onCloseModal={onCloseModal}>
       <div className="modal-header">
@@ -19,33 +58,79 @@ const AgreeModal = ({ onCloseModal, onOpenSignup }: AgreeModalProps) => {
         <p className="modal-title">이용악관 동의</p>
       </div>
       <label className="modal-all-agree">
-        <input type="checkbox" className="modal-all-agree-checkbox" />
+        <input
+          type="checkbox"
+          className="modal-all-agree-checkbox"
+          checked={agree1 && agree2 && agree3}
+          onChange={() => {
+            setAllAgree(!allAgree);
+            allAgree && agree1 ? setAgree1(false) : setAgree1(true);
+            allAgree && agree1 ? setAgree2(false) : setAgree2(true);
+            allAgree && agree1 ? setAgree3(false) : setAgree3(true);
+            setAgreeError("");
+          }}
+        />
         <p>모두 동의합니다.</p>
       </label>
       <div className="modal-agree-list">
         <div className="modal-agree-item">
           <div className="modal-agree-item-title">
-            <input type="checkbox" className="modal-agree-checkbox" />
+            <input
+              type="checkbox"
+              className="modal-agree-checkbox"
+              onChange={() => {
+                setAgree1(!agree1);
+                setAgreeError("");
+              }}
+              checked={agree1}
+            />
             <p className="modal-agree-sub">(필수) 만 14세 이상입니다</p>
           </div>
           <img src={arrow_right} alt="화살표" />
         </div>
         <div className="modal-agree-item">
           <div className="modal-agree-item-title">
-            <input type="checkbox" className="modal-agree-checkbox" />
+            <input
+              type="checkbox"
+              className="modal-agree-checkbox"
+              checked={agree2}
+              onChange={() => {
+                setAgree2(!agree2);
+                setAgreeError("");
+              }}
+            />
             <p className="modal-agree-sub">(필수) 모아바 이용약관 동의</p>
           </div>
           <img src={arrow_right} alt="화살표" />
         </div>
         <div className="modal-agree-item">
           <div className="modal-agree-item-title">
-            <input type="checkbox" className="modal-agree-checkbox" />
+            <input
+              type="checkbox"
+              className="modal-agree-checkbox"
+              checked={agree3}
+              onChange={() => {
+                setAgree3(!agree3);
+                setAgreeError("");
+              }}
+            />
             <p className="modal-agree-sub">(필수) 개인정보 이용약관 동의</p>
           </div>
           <img src={arrow_right} alt="화살표" />
         </div>
       </div>
-      <button className="modal-agree-button">가입하기</button>
+      {agreeError && <p className="modal-error">{agreeError}</p>}
+      <button
+        className={
+          active
+            ? "modal-agree-button"
+            : `modal-agree-button modal-agree-button-disable 
+            ${agreeBtnShake ? "shake" : ""}`
+        }
+        onClick={handleAgreeClick}
+      >
+        가입하기
+      </button>
     </Modal>
   );
 };
