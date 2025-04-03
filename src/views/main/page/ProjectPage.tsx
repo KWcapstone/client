68489c1193ccda093f619f2a40a690a4207463f3
Eunjin Oh <ibsuny3214@naver.com>
2025-04-03 21:24:11 +0900

    Fix: 각 요소에 클래스 네임 부여 (#4)

--- src/views/main/page/ProjectPage.tsx
+++ src/views/main/page/ProjectPage.tsx
@@ -20,114 +20,190 @@ const ProjectPage = () => {
       <div className="project-wrap">
         <div className="nevigation-wrap">
           <div className="title-wrap">
-            <h2>전체 회의</h2>
+            <h2 className="title-wrap-h2">전체 회의</h2>
             <div className="search-wrap">
-              <input type="text" placeholder="회의명 검색" />
+              <input
+                type="text"
+                placeholder="회의명 검색"
+                className="search-input"
+              />
               <Link to="/meeting">새로 만들기</Link>
             </div>
           </div>
           <div className="sort-wrap">
             <div className="tab-wrap">
-              <ul>
-                <li className={tab === "all" ? "active" : ""} onClick={() => setTab("all")}>전체</li>
-                <li className={tab === "mine" ? "active" : ""} onClick={() => setTab("mine")}>내 회의</li>
-                <li className={tab === "invite" ? "active" : ""} onClick={() => setTab("invite")}>초대된 회의</li>
+              <ul className="tab-ul">
+                <li
+                  className={tab === "all" ? "tab-li active" : "tab-li"}
+                  onClick={() => setTab("all")}
+                >
+                  전체
+                </li>
+                <li
+                  className={tab === "mine" ? "tab-li active" : "tab-li"}
+                  onClick={() => setTab("mine")}
+                >
+                  내 회의
+                </li>
+                <li
+                  className={tab === "invite" ? "tab-li active" : "tab-li"}
+                  onClick={() => setTab("invite")}
+                >
+                  초대된 회의
+                </li>
               </ul>
             </div>
             <div className="order-wrap">
-              <button onClick={() => setShowOrder(!showOrder)}>{ order ? "최신순" : "오래된 순" }<img src={arrow}/></button>
-              {
-                showOrder &&
-                <ul>
-                  <li onClick={() => {setOrder(true); setShowOrder(false)}}>최신순</li>
-                  <li onClick={() => {setOrder(false); setShowOrder(false)}}>오래된 순</li>
+              <button
+                onClick={() => setShowOrder(!showOrder)}
+                className="order-button"
+              >
+                {order ? "최신순" : "오래된 순"}
+                <img src={arrow} className="order-img" />
+              </button>
+              {showOrder && (
+                <ul className="order-ul">
+                  <li
+                    onClick={() => {
+                      setOrder(true);
+                      setShowOrder(false);
+                    }}
+                    className="order-li"
+                  >
+                    최신순
+                  </li>
+                  <li
+                    onClick={() => {
+                      setOrder(false);
+                      setShowOrder(false);
+                    }}
+                    className="order-li"
+                  >
+                    오래된 순
+                  </li>
                 </ul>
-              }
+              )}
             </div>
           </div>
         </div>
         <div className="card-wrap">
           <div className="card">
-            <img src={test} alt="" />
+            <img src={test} alt="" className="card-img" />
             <div className="info-wrap">
               <div className="title-wrap">
                 <div className="title">모아바 회의</div>
                 <div className="date">2025/1/16</div>
               </div>
               <div className="owner-wrap">
-                <div className="owner"><img src={test}/>모아바</div>
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
               </div>
             </div>
           </div>
           <div className="card">
-            <img src={test} alt="" />
+            <img src={test} alt="" className="card-img" />
             <div className="info-wrap">
               <div className="title-wrap">
                 <div className="title">모아바 회의</div>
                 <div className="date">2025/1/16</div>
               </div>
               <div className="owner-wrap">
-                <div className="owner"><img src={test}/>모아바</div>
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
               </div>
             </div>
           </div>
           <div className="card">
-            <img src={test} alt="" />
+            <img src={test} alt="" className="card-img" />
             <div className="info-wrap">
               <div className="title-wrap">
                 <div className="title">모아바 회의</div>
                 <div className="date">2025/1/16</div>
               </div>
               <div className="owner-wrap">
-                <div className="owner"><img src={test}/>모아바</div>
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
               </div>
             </div>
           </div>
           <div className="card">
-            <img src={test} alt="" />
+            <img src={test} alt="" className="card-img" />
             <div className="info-wrap">
               <div className="title-wrap">
                 <div className="title">모아바 회의</div>
                 <div className="date">2025/1/16</div>
               </div>
               <div className="owner-wrap">
-                <div className="owner"><img src={test}/>모아바</div>
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
               </div>
             </div>
           </div>
           <div className="card">
-            <img src={test} alt="" />
+            <img src={test} alt="" className="card-img" />
             <div className="info-wrap">
               <div className="title-wrap">
                 <div className="title">모아바 회의</div>
                 <div className="date">2025/1/16</div>
               </div>
               <div className="owner-wrap">
-                <div className="owner"><img src={test}/>모아바</div>
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
               </div>
             </div>
           </div>
           <div className="card">
-            <img src={test} alt="" />
+            <img src={test} alt="" className="card-img" />
             <div className="info-wrap">
               <div className="title-wrap">
                 <div className="title">모아바 회의</div>
                 <div className="date">2025/1/16</div>
               </div>
               <div className="owner-wrap">
-                <div className="owner"><img src={test}/>모아바</div>
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
               </div>
             </div>
           </div>
           <div className="card">
-            <img src={test} alt="" />
+            <img src={test} alt="" className="card-img" />
             <div className="info-wrap">
               <div className="title-wrap">
                 <div className="title">모아바 회의</div>
                 <div className="date">2025/1/16</div>
               </div>
               <div className="owner-wrap">
-                <div className="owner"><img src={test}/>모아바</div>
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="card">
+            <img src={test} alt="" className="card-img" />
+            <div className="info-wrap">
+              <div className="title-wrap">
+                <div className="title">모아바 회의</div>
+                <div className="date">2025/1/16</div>
+              </div>
+              <div className="owner-wrap">
+                <div className="owner">
+                  <img src={test} className="owner-img" />
+                  모아바
+                </div>
               </div>
             </div>
           </div>
