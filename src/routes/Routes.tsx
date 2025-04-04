22a537495ec27f13b56d132228c7a2f3e4ee4687
Eunjin Oh <ibsuny3214@naver.com>
2025-04-04 13:56:09 +0900

    Feat: 미팅페이지 생성 및 라우팅 (#6)

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import ProjectPage from "@/views/main/page/ProjectPage";
 import SplashPage from "@/views/splash/page/SplashPage";
-import RecordPage from '@/views/main/page/RecordPage';
-import SummaryPage from '@/views/main/page/SummaryPage';
-
+import RecordPage from "@/views/main/page/RecordPage";
+import SummaryPage from "@/views/main/page/SummaryPage";
+import MeetingPage from "@/views/meeting/page/MeetingPage";
 
 const Router = () => {
   return (
@@ -14,7 +14,7 @@ const Router = () => {
         <Route path="/project" element={<ProjectPage />} />
         <Route path="/record" element={<RecordPage />} />
         <Route path="/summary" element={<SummaryPage />} />
-        {/* <Route path="/meeting" element={</>}/> */}
+        <Route path="/meeting" element={<MeetingPage />} />
       </Routes>
     </BrowserRouter>
   );
