cd5fb3eddb7af01f7e1f3021f447c330dcaa524b
Eunjin Oh <ibsuny3214@naver.com>
2025-05-11 21:07:57 +0900

    Design: aside 제작 (#6)

--- src/views/meeting/page/MeetingPage.tsx
+++ src/views/meeting/page/MeetingPage.tsx
@@ -3,11 +3,13 @@ import "@/views/meeting/style/meeting.sass";
 import arrow from "@/assets/imgs/icon/arrow_down_black.svg";
 import test from "@/assets/imgs/common/test.png";
 import HeaderBar from "../components/HeaderBar";
+import SideBar from "../components/SideBar";
 
 const MeetingPage = () => {
   return (
     <>
       <HeaderBar />
+      <SideBar />
 
       <div className="project-content">
         <h1>회의 페이지</h1>
