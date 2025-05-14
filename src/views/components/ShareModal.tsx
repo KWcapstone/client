6c1d0bc6a87b3d2cae1114137241b852287dc65e
Eunjin Oh <ibsuny3214@naver.com>
2025-05-15 05:03:44 +0900

    Design: share modal (#6)

--- src/views/components/ShareModal.tsx
+++ src/views/components/ShareModal.tsx
@@ -1,5 +1,7 @@
-import "@/views/main/style/user-modal.sass";
+import "@/views/components/style/share-modal.sass";
 import Modal from "@/views/components/modal";
+import linkImg from "@/assets/imgs/icon/line-md_link.svg";
+import user from "@/assets/imgs/common/user.svg";
 
 interface ShareModalProps {
   onCloseModal: () => void;
@@ -10,28 +12,49 @@ const ShareModal = ({ onCloseModal }: ShareModalProps) => {
     <Modal onCloseModal={onCloseModal}>
       <p className="modal-title">공유하기</p>
       <div className="modal-wrap">
-        <div className="file-wrap">
-          <h3>파일 선택</h3>
-          <div className="file-wrap">
-            <div className="mind-map-wrap">
-              <div className="img-wrap"></div>
-              <p>마인드맵</p>
-            </div>
-            <div className="recoed-wrap">
-              <div className="img-wrap"></div>
-              <p>요약본</p>
-            </div>
-            <div className="summary-wrap">
-              <div className="img-wrap"></div>
-              <p>음성・스크립트</p>
+        <div className="email">
+          <span className="email-title">이메일 초대</span>
+          <div className="email-input-wrap">
+            <input
+              type="text"
+              className="email-input"
+              placeholder="이메일을 입력해주세요"
+            />
+            <button className="email-btn cursor-pointer">초대하기</button>
+          </div>
+        </div>
+        <div className="link">
+          <span className="link-title">초대 링크</span>
+          <div className="link-copy-wrap">
+            <div className="link-copy">moaba.com/cjy-zvq-qqux</div>
+            <div className="link-copy-btn-wrap cursor-pointer">
+              <img
+                src={linkImg}
+                className="link-copy-btn-icon"
+                alt="링크이미지"
+              />
+              <div className="link-copy-btn-text">복사하기</div>
             </div>
           </div>
         </div>
-        <div className="type-wrap">
-          <select>
-            <option>JPG 이미지(.jpg)</option>
-            <option>PNG 이미지(.png)</option>
-          </select>
+        <div className="member">
+          <span className="member-title">공유된 멤버</span>
+          <div className="member-list">
+            <div className="member-item">
+              <div className="member-info-wrap">
+                <img src={user} alt="user" className="member-img" />
+                <span className="member-name">이름</span>
+              </div>
+              <div className="member-role">회의 생성자</div>
+            </div>
+            <div className="member-item">
+              <div className="member-info-wrap">
+                <img src={user} alt="user" className="member-img" />
+                <span className="member-name">이름</span>
+              </div>
+              <div className="member-role">참석자</div>
+            </div>
+          </div>
         </div>
       </div>
     </Modal>
