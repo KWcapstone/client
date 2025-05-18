cec2cf997ea63be7e244e18eeb55a8572f59c506
gusrns48 <gusrns48@naver.com>
2025-05-18 16:11:57 +0900

    Chore: meeting mindmap design

--- src/views/meeting/components/MindMapComponent.tsx
+++ src/views/meeting/components/MindMapComponent.tsx
@@ -1,10 +1,15 @@
 import { ReactFlow } from '@xyflow/react';
 
+// style
+import '@xyflow/react/dist/style.css';
+import "@/views/meeting/style/mind-map.sass";
+
 // component
 import UseSpeechToText from "@/views/meeting/components/UseSpeechToText";
 
-import '@xyflow/react/dist/style.css';
-import "@/views/meeting/style/mind-map.sass";
+// import
+// import { useState, useEffect } from "react";
+
 
 const MindMapComponent = () => {
   const {
@@ -17,6 +22,8 @@ const MindMapComponent = () => {
     audioUrl,
   } = UseSpeechToText();
 
+  // const [mode, setMode] = useState<string>("none");
+
   const initialNodes = [
     { id: '1', position: { x: 0, y: 0 }, data: { label: '2' } },
     { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
@@ -28,32 +35,43 @@ const MindMapComponent = () => {
    
   return (
     <div className="mind-map-container">
-      <div className='mind-map-wrap'>
-        <ReactFlow nodes={initialNodes} edges={initialEdges} />
-        <ReactFlow nodes={initialNodes} edges={initialEdges} />
-      </div>
-      <div className="middle-bar">
-        <div className="record-length-wrap">
-          <div className="box-wrap">
-            <div className='red box'></div>
-            {Array.from({ length: 9 }).map(() => (
-              <div className='box'></div>
-            ))}
+      {
+        !isRecording ? (
+          <div className='start-wrap'>
+            <p>녹음을 시작하고 자동으로 생성되는<br/>마인드맵을 확인해보세요.</p>
+            <button className='btn-mic' onClick={toggleListening}>녹음 시작하기</button>
           </div>
-          <div className="box-time">
-            13:28
+        ) : (
+          <div className="mind-map-main">
+            <div className='mind-map-wrap'>
+              <ReactFlow nodes={initialNodes} edges={initialEdges} />
+              <ReactFlow nodes={initialNodes} edges={initialEdges} />
+            </div>
+            <div className="middle-bar">
+              <div className="record-length-wrap">
+                <div className="box-wrap">
+                  <div className='red box'></div>
+                  {Array.from({ length: 9 }).map(() => (
+                    <div className='box'></div>
+                  ))}
+                </div>
+                <div className="box-time">
+                  13:28
+                </div>
+                <div className="box-menu">
+                  {isRecording && !isPaused && (
+                    <button className='btn-pause' onClick={pauseRecording}>일시정지</button>
+                  )}
+                  {isRecording && isPaused && (
+                    <button className='btn-resume' onClick={resumeRecording}>▶️ 재개</button>
+                  )}
+                  <button className='btn-record' onClick={toggleListening}>{isRecording ? "정지" : "시작"}</button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-            {isRecording && !isPaused && (
-              <button onClick={pauseRecording}>⏸️</button>
-            )}
-            {isRecording && isPaused && (
-              <button onClick={resumeRecording}>▶️ 재개</button>
-            )}
-            <button onClick={toggleListening}>{isRecording ? "정지" : "시작"}</button>
-          </div>
-        </div>
-      </div>
+        )
+      }
     </div>
   );
 }
