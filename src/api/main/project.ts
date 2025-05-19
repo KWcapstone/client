761596bf168341d4aafcad08199f390ed8dd5594
Eunjin Oh <ibsuny3214@naver.com>
2025-05-19 22:28:58 +0900

    Feat: 프로젝트 전체 삭제 api 연결 (#24)

--- src/api/main/project.ts
+++ src/api/main/project.ts
@@ -1,10 +1,19 @@
 import Send from "@/api/send.ts";
 import { selectParams } from "@/types/selectParams";
+import { projects } from "@/types/deleteProject";
 
 export const getProject = (params: selectParams) => {
   return Send({
     method: "get",
     url: "main",
     params: params,
+  });
+};
+
+export const deleteProject = (data: projects[]) => {
+  return Send({
+    method: "delete",
+    url: "main/project/delete",
+    data: data,
   });
 };
---
+++ src/types/deleteProject.ts
@@ -0,0 +1,4 @@
+export interface projects {
+  projectId: Array<string>;
+  type: string;
+}
