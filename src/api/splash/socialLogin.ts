d1636a3a1b0da06f35b3e49fa51abc42bbd17bda
Eunjin Oh <ibsuny3214@naver.com>
2025-09-04 15:08:09 +0900

    Feat: 카카오 로그인 구현 완료 #40

--- src/api/splash/socialLogin.ts
+++ src/api/splash/socialLogin.ts
@@ -3,7 +3,6 @@ import Send from "@/api/send.ts";
 export const postKakaoLogin = (code: string) => {
   return Send({
     method: "get",
-    url: "/auth/login/kakao",
-    data: { code },
+    url: `/auth/login/kakao?code=${code}`,
   });
 };
